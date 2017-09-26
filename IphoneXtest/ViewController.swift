3765155d8f27b2c7daf79f908cbe1c3a2c96878e
xingbo <xingbo-rj@ofo.com>
2017-09-26 10:41:32 +0800

    Initial Commit

---
+++ IphoneXtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IphoneXtest
+//
+//  Created by xingbo on 2017/9/26.
+//  Copyright © 2017年 xingbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
